11cfcc60942c716dc2b369e6fdfeef637476e33f
Marc Jian Dasalla <dasallamarcjian@gmail.com>
2025-05-31 00:57:37 +0800

    postgre

--- taskflow/settings.py
+++ taskflow/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = "taskflow.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "taskflow",
+        "USER": "postgres",
+        "PASSWORD": "Jian021504",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
